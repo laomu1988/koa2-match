6b4ad388c8c1c8afc5e3edca3e1086eee6a77620
muzhilong <laomu1988@qq.com>
2017-03-07 14:17:12 +0800

    standardjs；回调包含多个程序

--- src/index.js
+++ src/index.js
@@ -1,88 +1,78 @@
 /**
  * 匹配处理程序
  * */
-require("babel-core/register");
-require("babel-polyfill");
-var uuid = require('uuid');
+require('babel-core/register')
+require('babel-polyfill')
+var uuid = require('uuid')
 
-var _ = require('lodash');
-var mixin = require('mixin-attr');
-const rules = [];
+var _ = require('lodash')
+var mixin = require('mixin-attr')
+const rules = []
 /**
  * 增加匹配规则和回调
  * */
-function match(rule, callback) {
+function match (rule, callback) {
   if (_.isArray(rule)) {
-    rule.forEach(r => match(r.rule, r.callback));
-  }
-  else if (arguments.length === 1) {
-    callback = rule;
-    rule = /\*/g;
+    rule.forEach(r => match(r.rule, r.callback))
+  } else if (arguments.length === 1) {
+    callback = rule
+    rule = /\*/g
     rules.push({rule, callback, id: uuid()})
-  }
-  else if (_.isString(rule) || _.isFunction(rule) || _.isRegExp(rule)) {
-    rule = {url: rule};
+  } else if (_.isString(rule) || _.isFunction(rule) || _.isRegExp(rule)) {
+    rule = {url: rule}
     rules.push({rule, callback, id: uuid()})
-  }
-  else if (rule && callback) {
+  } else if (rule && callback) {
     rules.push({rule, callback, id: uuid()})
   } else {
-    console.log('unkonw rule', rule, callback);
+    console.log('unkonw rule', rule, callback)
   }
 }
 
-function setRules(_rules) {
-  clean();
+function setRules (_rules) {
+  clean()
   if (_.isArray(_rules)) {
     _rules.forEach(function (r) {
       if (r && r.rule && r.callback) {
-        r.id = r.id || uuid();
-        rules.push(r);
+        r.id = r.id || uuid()
+        rules.push(r)
       }
-    });
+    })
   }
 }
 /**
  * 清空所有规则 todo:
  * */
-function clean() {
-  rules.splice(0, rules.length);
+function clean () {
+  rules.splice(0, rules.length)
 }
 
-async function ctx_handle(ctx, handle) {
+async function ctxHandles (ctx, handles, index) {
+  index = index || 0
+  if (index >= handles.length) return
+  await ctxHandle(ctx, handles[index])
+  await ctxHandles(ctx, handles, index + 1)
+}
+
+async function ctxHandle (ctx, handle) {
   if (_.isArray(handle)) {
-    // todo: 假如是promise,则顺序执行
-    handle.forEach(h => ctx_handle(ctx, h));
-  }
-  else if (_.isFunction(handle)) {
-    return await handle(ctx);
-  }
-  else if (_.isPlainObject(handle)) {
-    ctx_plain_change(ctx, handle);
+    await ctxHandles(ctx, handle)
+  } else if (_.isFunction(handle)) {
+    await handle(ctx)
+  } else if (_.isPlainObject(handle)) {
+    await ctxChangeByPlain(ctx, handle)
   }
 }
-/**
- * 直接修改ctx属性
- * todo:
- *  {
- *      'body': 'test',
- *      'request.header.cookie': ''
- *      'request': {
- *          'header': {
- *              'host': ''
- *          }
- *      }
- *  }
- */
-function ctx_plain_change(ctx, plainObject) {
+
+// 根据plainObject， 修改ctx的属性
+function ctxChangeByPlain (ctx, plainObject) {
   for (let attr in plainObject) {
-    let val = plainObject[attr];
-    attr = (attr + '').trim().toLowerCase();
+    let val = plainObject[attr]
+    attr = (attr + '').trim().toLowerCase()
     switch (attr) {
       case 'url':
       case 'method':
-        ctx.request[attr] = val;
-        continue;
+        ctx.request[attr] = val
+        continue
       case 'host':
       case 'hostname':
       case 'referer':
@@ -93,126 +83,123 @@ function ctx_plain_change(ctx, plainObject) {
       case 'accept-control-expose-headers':
       case 'cache-control':
       case 'cookie':
-        ctx.request.set(attr, val);
-        continue;
+        ctx.request.set(attr, val)
+        continue
       case 'body':
       case 'status':
-        ctx.response[attr] = val;
-        continue;
+        ctx.response[attr] = val
+        continue
     }
     if (attr.indexOf('.') < 0) {
       if (typeof ctx[attr] === 'object') {
         // 当是对象时,不要直接赋值,不然会影响对象上原有的属性
         // console.log('mixin:', attr, val);
-        mixin(ctx[attr], val);
+        mixin(ctx[attr], val)
         // console.log('result:', ctx[attr]);
       } else {
-        ctx[attr] = val;
+        ctx[attr] = val
       }
-      continue;
+      continue
     }
-    let attrs = attr.split('.');
-    let start = attrs.shift();
-    let end = attrs.join('.');
-    let reset = {};
-    reset[end] = val;
+    let attrs = attr.split('.')
+    let start = attrs.shift()
+    let end = attrs.join('.')
+    let reset = {}
+    reset[end] = val
     if (typeof ctx[start] === 'object') {
       // console.log('mixin2:', start, reset);
-      mixin(ctx[start], reset);
+      mixin(ctx[start], reset)
       // console.log('result:', ctx[start]);
     } else {
-      ctx[start] = reset;
+      ctx[start] = reset
     }
   }
 }
-
 
 /**
  * 返回koa中间件
  */
-function callback() {
+function callback () {
   return async function (ctx, next) {
     try {
-      await RunRuleHandle(ctx, rules.filter(r => (!r.rule.phase || r.rule.phase === 'request')));
-      await next();
-      await RunRuleHandle(ctx, rules.filter(r => (r.rule.phase === 'response')));
+      await RunRuleHandle(ctx, rules.filter(r => (!r.rule.phase || r.rule.phase === 'request')))
+      await next()
+      await RunRuleHandle(ctx, rules.filter(r => (r.rule.phase === 'response')))
       // console.log('ctx:', ctx);
-      resetHeader(ctx);
-    }
-    catch (e) {
-      console.log(e);
+      resetHeader(ctx)
+    } catch (e) {
+      console.log(e)
+      throw e
     }
   }
 }
 
-
-async function RunRuleHandle(ctx, list, nowIndex) {
-  nowIndex = nowIndex || 0;
-  if (nowIndex >= list.length) return Promise.resolve();
-  var r = list[nowIndex];
+async function RunRuleHandle (ctx, list, nowIndex) {
+  nowIndex = nowIndex || 0
+  if (nowIndex >= list.length) return Promise.resolve()
+  var r = list[nowIndex]
   if (isCtxMatchRule(ctx, r.rule)) {
-    ctx._matchs ? ctx._matchs.push(r.id) : ctx._matchs = [r.id];
-    return await ctx_handle(ctx, r.callback).then(function () {
+    ctx._matchs ? ctx._matchs.push(r.id) : ctx._matchs = [r.id]
+    return await ctxHandle(ctx, r.callback).then(function () {
       return RunRuleHandle(ctx, list, nowIndex + 1)
-    });
+    })
   }
-  return RunRuleHandle(ctx, list, nowIndex + 1);
+  return RunRuleHandle(ctx, list, nowIndex + 1)
 }
-
 
 module.exports.getRules = function () {
-  return rules;
-};
-module.exports.callback = callback;
-module.exports.match = match;
-module.exports.clean = clean;
-module.exports.setRules = setRules;
+  return rules
+}
+module.exports.callback = callback
+module.exports.match = match
+module.exports.clean = clean
+module.exports.setRules = setRules
 
-
-function isCtxMatchRule(ctx, rule) {
+function isCtxMatchRule (ctx, rule) {
   for (var key in rule) {
-    if (key === 'phase') continue;
-    var condition = rule[key];
+    if (key === 'phase') continue
+    var condition = rule[key]
     if (!TestOneRule(GetVal(ctx, key), condition)) {
-      return false;
+      return false
     }
   }
-  return true;
+  return true
 }
 
-function GetVal(ctx, key) {
+function GetVal (ctx, key) {
   switch (key) {
     case 'fullUrl':
-      var request = ctx.request;
+      var request = ctx.request
       if (request.url.indexOf('http') === 0) {
-        return request.url;
+        return request.url
       }
-      return request.protocol + '://' + request.header.host + request.url;
+      return request.protocol + '://' + request.header.host + request.url
     case 'url':
-      return ctx.request.url;
+      return ctx.request.url
     default:
-      var val = ctx.request[key];
+      var val = ctx.request[key]
       if (typeof val === 'string') {
-        return val.toLowerCase();
+        return val.toLowerCase()
       }
-      return ctx.get(key) || ctx[key];
+      return ctx.get(key) || ctx[key]
   }
 }
-function TestOneRule(val, rule) {
+function TestOneRule (val, rule) {
   if (_.isFunction(rule)) {
-    return rule(val);
+    return rule(val)
   } else if (_.isRegExp(rule)) {
-    return rule.test(val);
+    return rule.test(val)
   } else if (_.isString(rule)) {
-    return (val + '').indexOf(rule) >= 0;
+    return (val + '').indexOf(rule) >= 0
   }
-  return false;
+  return false
 }
 
 // response的header需要重新设置
-function resetHeader(ctx) {
-  var response = ctx.response, header = response.header;
+function resetHeader (ctx) {
+  var response = ctx.response
+  var header = response.header
   for (var attr in header) {
-    ctx.set(attr, header[attr]);
+    ctx.set(attr, header[attr])
   }
 }
--- test/base.js
+++ test/base.js
@@ -1,13 +1,12 @@
-var koa = require('koa');
-var app = new koa();
-var match = require('../lib/index.js');
+var Koa = require('koa')
+var app = new Koa()
+var match = require('../lib/index.js')
 
 match.match('test_str', function (ctx) {
-  ctx.response.body = 'test_str';
-});
+  ctx.response.body = 'test_str'
+})
 
-
-match.match('test_plain', {body: 'test_plain'});
+match.match('test_plain', {body: 'test_plain'})
 
 // 测试未通过原因: response的header不能直接赋值, 需要使用setHeader赋值
 match.match('test_plain_mixin', {
@@ -19,67 +18,66 @@ match.match('test_plain_mixin', {
       'server2': 'test-server2'
     }
   }
-});
+})
 
 // 一次添加多个匹配规则
 match.match([
   {
     rule: /test_arr/,
     callback: function (ctx) {
-      ctx.response.body = 'test_arr';
+      ctx.response.body = 'test_arr'
     }
   },
-  {rule: /test_plain2/, callback: {body: 'test2'}},
-]);
+  {rule: /test_plain2/, callback: {body: 'test2'}}
+])
 
 // promise
 match.match('test_promise1', function (ctx) {
   // console.log('is run promise1');
-  return new Promise(function (resolve, reject) {
+  return new Promise(function (resolve) {
     setTimeout(function () {
-      ctx.response.body = 'test_promise1';
-      resolve();
-    }, 100);
-  });
-});
+      ctx.response.body = 'test_promise1'
+      resolve()
+    }, 100)
+  })
+})
 
 // promise链
 match.match('test_promise_chain', function (ctx) {
   // console.log('is run test_promise_chain2.1');
-  return new Promise(function (resolve, reject) {
+  return new Promise(function (resolve) {
     setTimeout(function () {
-      ctx.response.body = 'test_promise_chain2.1';
-      resolve();
-    }, 100);
-  });
-});
+      ctx.response.body = 'test_promise_chain2.1'
+      resolve()
+    }, 100)
+  })
+})
 match.match('test_promise_chain', function (ctx) {
   // console.log('is run test_promise_chain2.2');
-  return new Promise(function (resolve, reject) {
+  return new Promise(function (resolve) {
     setTimeout(function () {
-      ctx.response.body = 'test_promise_chain2.2';
-      resolve();
-    }, 100);
-  });
-});
-
+      ctx.response.body = 'test_promise_chain2.2'
+      resolve()
+    }, 100)
+  })
+})
 
 match.match(function (ctx) {
-  console.log('url:', ctx.url);
-  if (!ctx.response.body) ctx.response.body = 'not_found';
-});
+  console.log('url:', ctx.url)
+  if (!ctx.response.body) ctx.response.body = 'not_found'
+})
 
-var middleware = match.callback();
+var middleware = match.callback()
 
-app.use(middleware);
+app.use(middleware)
 
-console.log('rules:', match.getRules());
+console.log('rules:', match.getRules())
 
 setTimeout(function () {
   // 程序启动后再添加规则是否可以
   match.match('test_add_later', {'body': 'test_add_later'})
-}, 200);
+}, 200)
 
 // app.listen(4000);
 
-module.exports = app;
+module.exports = app
